0328fcd2a6b8b246a1011fecfd17995177283b8c
amritbola <bolaa@oregonstate.edu>
2024-05-02 19:41:56 -0700

    Added database credentials

--- login-signup/db.php
+++ login-signup/db.php
@@ -1,3 +1,8 @@
 <?php 
-    $con = mysqli_connect("localhost", "root", "", "registration") or die(mysql_error());
+    $DBhost = "localhost";
+    $DBusername = "root";
+    $DBpassword = "";
+    $DBname = "registration";
+
+    $con = mysqli_connect($DBhost, $DBusername, $DBpassword, $DBname) or die(mysql_error());
 ?>
