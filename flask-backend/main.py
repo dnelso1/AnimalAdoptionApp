ef5eb358a37264504f67201023693eb5ec2de82b
abigailrgray <grayabi@oregonstate.edu>
2024-05-16 21:30:25 -0400

    added route for adding an animal profiles and profile pictures to the database

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -4,12 +4,14 @@ import logging
 import os
 
 from flask import Flask, request
+from flask_cors import CORS
 
 import sqlalchemy
 
 from connect_connector import connect_with_connector
 
 app = Flask(__name__)
+CORS(app)
 
 logger = logging.getLogger()
 
@@ -46,6 +48,45 @@ def get_shelter_names():
         for row in rows:
             shelter_names.append(row._asdict()["shelter_name"])
         return {'shelter_names': shelter_names}
+
+@app.route('/add-animal-profile', methods=['POST'])
+def add_animal_profile():
+    with db.connect() as conn:
+        # Get animal profile data
+        content = request.get_json()
+
+        print(content)
+        stmt = sqlalchemy.text(
+            'INSERT INTO animal_profiles(shelter_id, name, type, breed, disposition, availability, news_item, description) '
+            'VALUES (:shelter_id, :name, :type, :breed, :disposition, :availability, :news_item, :description)'
+        )
+        conn.execute(stmt, parameters={'shelter_id': 1,
+                                       'name': content['name'],
+                                       'type': content['type'],
+                                       'breed': content['breed'],
+                                       'disposition': content['disposition'],
+                                       'availability': content['availability'],
+                                       'news_item': content['news_blurb'],
+                                       'description': content['description']})
+        stmt2 = sqlalchemy.text('SELECT last_insert_id()')
+        id = conn.execute(stmt2).scalar()
+
+        picture_urls = []
+        for url in content['images']:
+            if url != '':
+                picture_urls.append(url)
+
+        stmt3 = sqlalchemy.text(
+                'INSERT INTO animal_pictures(picture_url, animal_id) '
+                'VALUES (:picture_url, :animal_id)'
+            )
+        for url in picture_urls:
+            conn.execute(stmt3, parameters={'picture_url': url,
+                                            'animal_id': id})
+
+        conn.commit()
+        return ({'id': id})
+
 
 
 
